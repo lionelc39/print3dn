46527df1a47b2cc6929f51856dec48aa29f8f392
lionelc39 <lionelc39@gmail.com>
2025-11-22 13:48:00 -0300

    Update Catalogo.jsx

--- src/Catalogo.jsx
+++ src/Catalogo.jsx
@@ -142,7 +142,7 @@ const Catalogo = () => {
                 src={producto.imagen}
                 alt={producto.nombre}
                 className={`w-full h-48 ${
-                  producto.id === 5 || producto.id === 7 || producto.id === 9 || producto.id === 10
+                  producto.id === 5 || producto.id === 7 |
                     ? 'object-contain bg-gray-50 p-2' 
                     : 'object-cover'
                 }`}
